39decc373b6b35315c0bcb94c51d5656227c4a33
Chelsea-san <chelseagabriellag@gmail.com>
2024-08-25 11:48:40 +0100

    new changes

---
+++ script.js
@@ -0,0 +1,10 @@
+function showSidebar() {
+    const sidebar = document.querySelector('.side-bar')
+    sidebar.style.display = 'flex'
+}
+
+
+function hideSidebar(){
+    const sidebar = document.querySelector('.side-bar')
+    sidebar.style.display = 'none'
+}

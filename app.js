b76e0a463850e982fe6c7cebbcc19e91b417038b
Successdanesy <successdanesy@gmail.com>
2024-08-29 18:43:55 +0100

    darkened images for the home page, and worked on the homepage, added app.JS for the javascript of the home page.

---
+++ app.js
@@ -0,0 +1,67 @@
+let items = document.querySelectorAll('.slider .list .item');
+let next = document.getElementById('next');
+let prev = document.getElementById('prev');
+let thumbnails = document.querySelectorAll('.thumbnail .item');
+
+// config param
+let countItem = items.length;
+let itemActive = 0;
+// event next click
+next.onclick = function(){
+    itemActive = itemActive + 1;
+    if(itemActive >= countItem){
+        itemActive = 0;
+    }
+    showSlider();
+}
+//event prev click
+prev.onclick = function(){
+    itemActive = itemActive - 1;
+    if(itemActive < 0){
+        itemActive = countItem - 1;
+    }
+    showSlider();
+}
+// auto run slider
+let refreshInterval = setInterval(() => {
+    next.click();
+}, 5000)
+function showSlider(){
+    // remove item active old
+    let itemActiveOld = document.querySelector('.slider .list .item.active');
+    let thumbnailActiveOld = document.querySelector('.thumbnail .item.active');
+    itemActiveOld.classList.remove('active');
+    thumbnailActiveOld.classList.remove('active');
+
+    // active new item
+    items[itemActive].classList.add('active');
+    thumbnails[itemActive].classList.add('active');
+
+    // clear auto time run slider
+    clearInterval(refreshInterval);
+    refreshInterval = setInterval(() => {
+        next.click();
+    }, 5000)
+}
+
+// click thumbnail
+thumbnails.forEach((thumbnail, index) => {
+    thumbnail.addEventListener('click', () => {
+        itemActive = index;
+        showSlider();
+    })
+})
+
+//Beginning of JS for sidebar
+      function showSidebar(){
+        const sidebar = document.querySelector('.sidebar');
+        sidebar.style.right = '0'; // Slide in from right
+        document.body.classList.add('sidebar-open'); // Add class to hide menu button
+      }
+    
+      function hideSidebar(){
+        const sidebar = document.querySelector('.sidebar');
+        sidebar.style.right = '-250px'; // Slide out to the right
+        document.body.classList.remove('sidebar-open'); // Remove class to show menu button again
+      }
+  //End of JS
